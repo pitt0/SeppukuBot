91c2cd21a61e4f2da3b3d519c32bc6c378548687
pitt0 <samuelebaffo32@gmail.com>
2022-12-23 14:57:31 +0100

    Added load method

--- games/leaderboard.py
+++ games/leaderboard.py
@@ -11,6 +11,12 @@ class Leaderboard:
     def __init__(self, game: str):
         self.path.replace("_", game)
 
+    @classmethod
+    def load(cls, game: str):
+        path = cls.path.replace("_", game)
+        with open(path) as f:
+            return json.load(f)
+
     def __enter__(self):
         with open(self.path) as f:
             self.cache = json.load(f)
